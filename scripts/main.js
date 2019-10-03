c8c70f66dee9bd3a695914b61fa84f44223fd80e
Ian Patel <ian.patel@yahoo.com>
2019-10-03 00:08:44 -0400

    fix typo

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,11 @@ const regen_format = '&{template:default} {{name=%s}} {{%s=[[round([[%s]]*[[(%s)
 const percent_format = '&{template:default} {{name=%s}} {{%s=[[1d100cs>[[100-(%s)+1]]]]}}';
 
 
+function chat(msg, string) {
+    sendChat(msg.who, string);
+}
+
+
 function get_character_names(who, id) {
     const found_characters = [];
     Object.keys(characters_by_owner).forEach(function(character_name) {
@@ -41,8 +46,8 @@ function get_character_names(who, id) {
 }
 
 
-function get_character(who, id) {
-    const character_names = get_character_names(who, id);
+function get_character(msg) {
+    const character_names = get_character_names(msg.who, msg.id);
 
     let characters = [];
     for (let i = 0; i < character_names.length; i++) {
@@ -55,11 +60,11 @@ function get_character(who, id) {
     }
 
     if (characters.length === 0) {
-        sendChat(who, 'Error, did not find a character for ' + who);
+        chat(msg, 'Error, did not find a character for ' + msg.who);
         return null;
     } else if (characters.length > 1) {
         // warn, but pray we've got the right ones regardless
-        log('warning, found ' + characters.length + ' matching characters for ' + who);
+        log('warning, found ' + characters.length + ' matching characters for ' + msg.who);
     }
 
     return characters[0];
@@ -129,7 +134,7 @@ function generate_stat_roll_modifier_from_items(character, stat_to_roll, effect_
 
 
 function roll_stat(msg) {
-    const character = get_character(msg.who, msg.playerid);
+    const character = get_character(msg);
     if (character === null) {
         log('error, unknown character for ' + msg.who);
         return;
@@ -140,95 +145,95 @@ function roll_stat(msg) {
 
     switch (stat_to_roll) {
         case 'health':
-            sendChat(msg.who, total_format.format('Total Health', 'Health', modifier));
+            chat(msg, total_format.format('Total Health', 'Health', modifier));
             break;
         case 'stamina':
-            sendChat(msg.who, total_format.format('Total Stamina', 'Stamina', modifier));
+            chat(msg, total_format.format('Total Stamina', 'Stamina', modifier));
             break;
 
         case 'mana':
-            sendChat(msg.who, total_format.format('Total Mana', 'Mana', modifier));
+            chat(msg, total_format.format('Total Mana', 'Mana', modifier));
             break;
 
         case 'health regeneration':
             const total_health = generate_stat_roll_modifier_from_items(character, 'health', 'stat');
-            sendChat(msg.who, regen_format.format('Health Regeneration', 'Regen', total_health, modifier));
+            chat(msg, regen_format.format('Health Regeneration', 'Regen', total_health, modifier));
             break;
 
         case 'stamina regeneration':
             const total_stamina = generate_stat_roll_modifier_from_items(character, 'stamina', 'stat');
-            sendChat(msg.who, regen_format.format('Stamina Regeneration', 'Regen', total_stamina, modifier));
+            chat(msg, regen_format.format('Stamina Regeneration', 'Regen', total_stamina, modifier));
             break;
 
         case 'mana regeneration':
             const total_mana = generate_stat_roll_modifier_from_items(character, 'mana', 'stat');
-            sendChat(msg.who, regen_format.format('Mana Regeneration', 'Regen', total_mana, modifier));
+            chat(msg, regen_format.format('Mana Regeneration', 'Regen', total_mana, modifier));
             break;
 
         case 'movement speed':
-            sendChat(msg.who, total_format.format('Total Movement Speed', 'Speed', modifier));
+            chat(msg, total_format.format('Total Movement Speed', 'Speed', modifier));
             break;
 
         case 'ac':
-            sendChat(msg.who, total_format.format('Total AC', 'AC', modifier));
+            chat(msg, total_format.format('Total AC', 'AC', modifier));
             break;
 
         case 'evasion':
-            sendChat(msg.who, percent_format.format('Evasion', 'Evasion', modifier));
+            chat(msg, percent_format.format('Evasion', 'Evasion', modifier));
             break;
 
         case 'magic resist':
-            sendChat(msg.who, total_format.format('Total Magic Resist', 'Magic Resist', modifier));
+            chat(msg, total_format.format('Total Magic Resist', 'Magic Resist', modifier));
             break;
 
         case 'condition resist':
-            sendChat(msg.who, percent_format.format('Condition Resist', 'CR', modifier));
+            chat(msg, percent_format.format('Condition Resist', 'CR', modifier));
             break;
 
         case 'melee damage':
-            sendChat(msg.who, total_format.format('Bonus Melee Damage', 'Damage', modifier));
+            chat(msg, total_format.format('Bonus Melee Damage', 'Damage', modifier));
             break;
 
         case 'ranged fine damage':
-            sendChat(msg.who, total_format.format('Bonus Ranged/Fine Damage', 'Damage', modifier));
+            chat(msg, total_format.format('Bonus Ranged/Fine Damage', 'Damage', modifier));
             break;
 
         case 'magic damage':
-            sendChat(msg.who, total_format.format('Bonus Magic Damage', 'Damage', modifier));
+            chat(msg, total_format.format('Bonus Magic Damage', 'Damage', modifier));
             break;
 
         case 'critical hit chance':
-            sendChat(msg.who, percent_format.format('Critical Hit Chance', 'Crit', modifier));
+            chat(msg, percent_format.format('Critical Hit Chance', 'Crit', modifier));
             break;
 
         case 'commands':
-            sendChat(msg.who, total_format.format('Total Commands', 'Commands', modifier));
+            chat(msg, total_format.format('Total Commands', 'Commands', modifier));
             break;
 
         case 'languages':
-            sendChat(msg.who, total_format.format('Total Languages', 'Languages', modifier));
+            chat(msg, total_format.format('Total Languages', 'Languages', modifier));
             break;
 
         case 'item efficiency':
-            sendChat(msg.who, total_format.format('Total Item Efficiency', 'Modifier', modifier));
+            chat(msg, total_format.format('Total Item Efficiency', 'Modifier', modifier));
             break;
 
         case 'buff limit':
-            sendChat(msg.who, total_format.format('Total Buff Limit', 'Buff Limit', modifier));
+            chat(msg, total_format.format('Total Buff Limit', 'Buff Limit', modifier));
             break;
 
         case 'concentration limit':
-            sendChat(msg.who, total_format.format('Total Concentration Limit', 'Concentration Limit', modifier));
+            chat(msg, total_format.format('Total Concentration Limit', 'Concentration Limit', modifier));
             break;
 
         default:
-            sendChat(msg.who, 'Error, unknown stat ' + stat_to_roll);
+            chat(msg, 'Error, unknown stat ' + stat_to_roll);
     }
 }
 
 
 function roll_skill(msg) {
-    const character = get_character(msg.who, msg.playerid);
+    const character = get_character(msg);
     if (character === null) {
         log('error, unknown character for ' + msg.who);
         return;
@@ -252,7 +257,7 @@ function roll_skill(msg) {
         roll_string += '+' + modifier;
     }
 
-    sendChat(msg.who, total_format.format(skill_to_roll, 'Roll', roll_string));
+    chat(msg, total_format.format(skill_to_roll, 'Roll', roll_string));
 }
 
 
